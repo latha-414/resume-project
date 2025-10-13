d921dbde8eb18d43ee5ff10921499ee13e85f9f3
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-10-13 15:11:37 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentList from './components/StudentList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Student Management</h1>
+      <StudentList />
+    </div>
+  );
+}
+
+export default App;
