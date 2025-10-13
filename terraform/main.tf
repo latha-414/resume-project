d1c50fa5dd1a4e66f4df9ea295136649aee9f7f9
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-10-13 17:47:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "ecs_sg" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
